35dcce1ac1d6e15e3c046f64d195ca507df28e0f
zhangzhiqiang <1036916439@qq.com>
2021-03-24 00:21:08 +0800

    六十六加一

---
+++ src/main/java/com/zzq/leetcode/一到一百/六十六加一.java
@@ -0,0 +1,23 @@
+package com.zzq.leetcode.一到一百;
+
+public class 六十六加一 {
+	public int[] plusOne(int[] digits) {
+		int cur=0;
+		for (int i = digits.length-1; i >=0; i--) {
+			if(i==digits.length-1){
+				int num=digits[i]+1;
+				cur=num/10;
+				digits[i]=num%10;
+			}else {
+				int num =cur+digits[i];
+				digits[i]=num%10;
+				cur=num/10;
+			}
+		}
+		if(digits[0]==0){
+			digits = new int[digits.length+1];
+			digits[0]=1;
+		}
+		return digits;
+	}
+}
