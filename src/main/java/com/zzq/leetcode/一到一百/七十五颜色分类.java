34d5579d50c387114a5ced418367db288aac2480
zhangzhiqiang <zhangzhiqiang@didiglobal.com>
2021-03-24 13:05:03 +0800

    七十五颜色分类

---
+++ src/main/java/com/zzq/leetcode/一到一百/七十五颜色分类.java
@@ -0,0 +1,30 @@
+package com.zzq.leetcode.一到一百;
+
+/**
+ * @author zhangzhiqiang
+ * @date 2021-03-24 1:04 下午
+ */
+public class 七十五颜色分类 {
+
+  public void sortColors(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+    for (int i = 0; i <= right; i++) {
+      if (nums[i] == 0) {
+        swap(nums, i, left);
+        left++;
+      }
+      if (nums[i] == 2) {
+        swap(nums, i, right);
+        right--;
+        i--;
+      }
+    }
+  }
+
+  public void swap(int[] swap, int i, int j) {
+    int l = swap[i];
+    swap[i] = swap[j];
+    swap[j] = l;
+  }
+}
