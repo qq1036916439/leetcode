e0539a4b980f25747612432448bd3503b09bbe40
zhangzhiqiang <zhangzhiqiang@didiglobal.com>
2021-03-25 13:26:45 +0800

    一百零三十二二叉树的锯齿状遍历

---
+++ src/main/java/com/zzq/leetcode/一百到两百/一百零三二叉树的锯齿形遍历.java
@@ -0,0 +1,45 @@
+package com.zzq.leetcode.一百到两百;
+
+import com.zzq.leetcode.base.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author zhangzhiqiang
+ * @date 2021-03-25 1:25 下午
+ */
+public class 一百零三二叉树的锯齿形遍历 {
+
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (root == null) {
+      return res;
+    }
+    LinkedList<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    boolean falg = false;
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      LinkedList<Integer> list = new LinkedList<>();
+      for (int i = 0; i < size; i++) {
+        TreeNode poll = queue.poll();
+        if (falg) {
+          list.addFirst(poll.val);
+        } else {
+          list.addLast(poll.val);
+        }
+        if (poll.left != null) {
+          queue.addLast(poll.left);
+        }
+        if (poll.right != null) {
+          queue.addLast(poll.right);
+        }
+      }
+      res.add(list);
+      falg = !falg;
+    }
+    return res;
+  }
+}
---
+++ src/main/java/com/zzq/leetcode/二百到三百/二百三十六二叉树的最先公共祖先.java
@@ -0,0 +1,27 @@
+package com.zzq.leetcode.二百到三百;
+
+import com.zzq.leetcode.base.TreeNode;
+
+/**
+ * @author zhangzhiqiang
+ * @date 2021-03-25 1:21 下午
+ */
+public class 二百三十六二叉树的最先公共祖先 {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null) {
+      return null;
+    }
+    if (root == p || root == q) {
+      return root;
+    }
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
+    if (left == null) {
+      return right;
+    }
+    if (right == null) {
+      return left;
+    }
+    return root;
+  }
+}
