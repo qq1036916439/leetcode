6822bbb19ebf4e3da438d40b07e0717d1563592d
zhangzhiqiang <1036916439@qq.com>
2021-03-28 19:39:16 +0800

    一百二十八最长连续序列

---
+++ src/main/java/com/zzq/leetcode/一百到两百/一百二十八最长连续序列.java
@@ -0,0 +1,26 @@
+package com.zzq.leetcode.一百到两百;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 一百二十八最长连续序列 {
+	public int longestConsecutive(int[] nums) {
+		Set<Integer> set =new HashSet();
+		int res=0;
+		for(int i=0;i<nums.length;i++){
+			set.add(nums[i]);
+		}
+		for(int i=0;i<nums.length;i++){
+			if(!set.contains(nums[i]-1)){
+				int curNum=nums[i];
+				int length=0;
+				while(set.contains(curNum)){
+					curNum++;
+					length++;
+				}
+				res=Math.max(length,res);
+			}
+		}
+		return res;
+	}
+}
