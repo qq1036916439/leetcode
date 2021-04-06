c5ef39d3542cb7890e2709508f8e228cc7d3d5a5
zhangzhiqiang <zhangzhiqiang@didiglobal.com>
2021-04-06 13:27:45 +0800

    八十删除有序数组中的重复项

---
+++ src/main/com/zzq/leetcode/一到一百/二十六删除有序数组中的重复项.java
@@ -0,0 +1,18 @@
+package com.zzq.leetcode.一到一百;
+
+/**
+ * @author zhangzhiqiang
+ * @date 2021-04-06 1:19 下午
+ */
+public class 二十六删除有序数组中的重复项 {
+  public int removeDuplicates(int[] nums) {
+    int i=0;
+    for(int j=1;j<nums.length;j++){
+      if(nums[i]!=nums[j]){
+        i++;
+        nums[i]=nums[j];
+      }
+    }
+    return i+1;
+  }
+}
---
+++ src/main/com/zzq/leetcode/一到一百/八十删除有序数组中的重复项.java
@@ -0,0 +1,25 @@
+package com.zzq.leetcode.一到一百;
+
+/**
+ * @author zhangzhiqiang
+ * @date 2021-04-06 1:27 下午
+ */
+public class 八十删除有序数组中的重复项 {
+
+  public int removeDuplicates(int[] nums) {
+    int n = nums.length;
+    if (n <= 2) {
+      return n;
+    }
+    int slow = 2;
+    int fast = 2;
+    while (fast < n) {
+      if (nums[slow - 2] < nums[fast]) {
+        nums[slow] = nums[fast];
+        slow++;
+      }
+      fast++;
+    }
+    return slow;
+  }
+}
