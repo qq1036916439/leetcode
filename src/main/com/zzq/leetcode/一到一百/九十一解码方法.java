ee88a1b7c14a54e60c60ab60c12dc31ac7073b22
zhangzhiqiang <zhangzhiqiang@didiglobal.com>
2021-04-22 13:30:42 +0800

    九十一解码方法

--- src/main/com/zzq/leetcode/一到一百/九十一解码方法.java
+++ src/main/com/zzq/leetcode/一到一百/九十一解码方法.java
@@ -5,6 +5,7 @@ package com.zzq.leetcode.一到一百;
  * @date 2021-04-21 1:26 下午
  */
 public class 九十一解码方法 {
+  //todo 动态方程推到
   public int numDecodings(String s) {
     if (s.isEmpty() || s.charAt(0) == '0') {
       return 0;
