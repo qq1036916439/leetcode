ed35b217a2e42631354e22231c6e5e16d0ca3e29
zhangzhiqiang <zhangzhiqiang@didiglobal.com>
2021-06-03 15:23:55 +0800

    五百二十五连续数组

---
+++ src/main/com/zzq/leetcode/五百到六百/五百二十五连续数组.java
@@ -0,0 +1,29 @@
+package com.zzq.leetcode.五百到六百;
+
+import java.util.HashMap;
+
+/**
+ * @author zhangzhiqiang
+ * @date 2021-06-03 3:23 下午
+ */
+public class 五百二十五连续数组 {
+  public int findMaxLength(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int result = 0;
+    int sum = 0;
+    map.put(0, -1);
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == 0) {
+        sum++;
+      } else {
+        sum--;
+      }
+      if (map.containsKey(sum)) {
+        result = Math.max(i - map.get(sum), result);
+      } else {
+        map.put(sum, i);
+      }
+    }
+    return result;
+  }
+}
