25d4e56ad8cb491698cd17d4a2b7fb924b4f6788
qq1036916439 <1036916439@qq.com>
2021-06-07 11:28:21 +0800

    四百九十四目标和

---
+++ src/main/com/zzq/leetcode/四百到五百/四百九十四目标和.java
@@ -0,0 +1,20 @@
+package com.zzq.leetcode.四百到五百;
+
+public class 四百九十四目标和 {
+	int res = 0;
+	public int findTargetSumWays(int[] nums, int target) {
+		back(nums, 0, target, 0);
+		return res;
+	}
+
+	public void back(int[] nums, int index, int target, int sum) {
+		if (nums.length == index) {
+			if (sum == target) {
+				res++;
+			}
+			return;
+		}
+		back(nums, index + 1, target, sum - nums[index]);
+		back(nums, index + 1, target, sum + nums[index]);
+	}
+}
