ddfa45213aac8225cdb131b4e8b5932fccf82f79
zhangzhiqiang <zhangzhiqiang@didiglobal.com>
2021-07-14 12:50:31 +0800

    一百九十八打家劫舍

--- src/main/com/zzq/leetcode/一百到两百/一百九十八打家劫舍.java
+++ src/main/com/zzq/leetcode/一百到两百/一百九十八打家劫舍.java
@@ -10,7 +10,7 @@ public class 一百九十八打家劫舍 {
 		}
 		int[] dp = new int[nums.length];
 		dp[0] = nums[0];
-		dp[1] = nums[0] > nums[1] ? nums[0] : nums[1];
+		dp[1] = Math.max(nums[0], nums[1]);
 		for (int i = 2; i < dp.length; i++) {
 			dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
 		}
