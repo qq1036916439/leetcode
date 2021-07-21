c935dc0a24a87ded50f94a95f94376ad0228d3b6
zhangzhiqiang <zhangzhiqiang@didiglobal.com>
2021-07-21 14:54:19 +0800

    跳跃游戏

--- src/main/com/zzq/leetcode/一到一百/五十五跳跃游戏.java
+++ src/main/com/zzq/leetcode/一到一百/五十五跳跃游戏.java
@@ -3,10 +3,13 @@ package com.zzq.leetcode.一到一百;
 public class 五十五跳跃游戏 {
 	public boolean canJump(int[] nums) {
 		int n = nums.length;
+		//跳跃最远距离
 		int rightmost = 0;
 		for (int i = 0; i < n; i++) {
+			//如果当前坐标 小于去更新下 跳跃最远距离
 			if (i <= rightmost) {
 				rightmost = Math.max(rightmost, i + nums[i]);
+				//如果跳跃的距离 大于长度的话，直接return true。
 				if (rightmost >= n - 1) {
 					return true;
 				}
