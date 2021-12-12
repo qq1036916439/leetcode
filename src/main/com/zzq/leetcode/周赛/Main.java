7e9020967e7b838dd465afe561a50d1d3f2b8e28
qq1036916439 <1036916439@qq.com>
2021-12-12 21:02:44 +0800

    周赛

--- src/main/com/zzq/leetcode/周赛/Main.java
+++ src/main/com/zzq/leetcode/周赛/Main.java
@@ -1,28 +1,249 @@
 package com.zzq.leetcode.周赛;
 
+import com.zzq.leetcode.base.TreeNode;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
 
 	public static void main(String[] args) {
-		countVowels("noosabasboosa");
 	}
 
-	public static long countVowels(String word) {
-		long counter = 0;
-		char[] chars = word.toCharArray();
-		for (char c : chars) {
-			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-				counter++;
+
+	public void dfs(int[][] fruits, int k, int index, int sum, HashSet<Integer> set) {
+		if (k <= 0) {
+			return;
+		}
+
+		int left = index;
+		while (left > 0 && fruits[index][0] - fruits[left][0] > k && !set.contains(left)) {
+			left--;
+			if (!set.contains(left)) {
+				sum = fruits[left][1];
+				break;
 			}
 		}
-		long n = chars.length * (chars.length + 1) / 2;
-		return counter * n / 2;
+		int right = index;
+		while (left > 0 && fruits[index][0] - fruits[right][0] > k && set.contains(right)) {
+			right++;
+			if (!set.contains(right)) {
+				sum = fruits[right][1];
+				break;
+			}
+		}
 	}
 
+
+	public static int minimumRefill(int[] plants, int capacityA, int capacityB) {
+		int n = plants.length;
+		int ans = 0;
+		int restA = capacityA;
+		int restB = capacityB;
+		int right = plants.length - 1;
+		for (int i = 0; i < n; i++) {
+			if (i == right) {
+				if (restA > restB) {
+					if (restA >= plants[i]) {
+						restA -= plants[i];
+					} else {
+						ans++;
+						restA = capacityA - plants[i];
+					}
+					plants[i] = 0;
+
+					if (restB >= plants[right]) {
+						restB -= plants[right];
+					} else {
+						ans++;
+						restB = capacityB - plants[right];
+					}
+					plants[right] = 0;
+					right--;
+				} else {
+					if (restB >= plants[right]) {
+						restB -= plants[right];
+					} else {
+						ans++;
+						restB = capacityB - plants[right];
+					}
+					plants[right] = 0;
+					right--;
+
+					if (restA >= plants[i]) {
+						restA -= plants[i];
+					} else {
+						ans++;
+						restA = capacityA - plants[i];
+					}
+					plants[i] = 0;
+				}
+				if (right == i) {
+					break;
+				}
+			} else {
+				if (restA >= plants[i]) {
+					restA -= plants[i];
+				} else {
+					ans++;
+					restA = capacityA - plants[i];
+				}
+				plants[i] = 0;
+
+				if (restB >= plants[right]) {
+					restB -= plants[right];
+				} else {
+					ans++;
+					restB = capacityB - plants[right];
+				}
+				plants[right] = 0;
+				right--;
+
+				if (right == i) {
+					break;
+				}
+			}
+		}
+		return ans;
+	}
+
+	public long subArrayRanges(int[] nums) {
+		long res = 0;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < nums.length; i++) {
+			max = Integer.MIN_VALUE;
+			min = Integer.MAX_VALUE;
+			for (int j = i; j < nums.length; j++) {
+				max = Math.max(max, nums[j]);
+				min = Math.min(min, nums[j]);
+				res = res + (max - min);
+			}
+		}
+		return res;
+	}
+
+	public static int countPoints(String rings) {
+		HashMap<String, HashSet<Character>> map = new HashMap<>();
+		char[] chars = rings.toCharArray();
+		for (int i = 0; i < chars.length; i += 2) {
+			HashSet<Character> set = map.getOrDefault(chars[i + 1] + "", new HashSet<>());
+			set.add(chars[i]);
+			map.put(chars[i + 1] + "", set);
+		}
+		Set<Map.Entry<String, HashSet<Character>>> entries = map.entrySet();
+		int count = 0;
+		for (Map.Entry<String, HashSet<Character>> entry : entries) {
+			if (entry.getValue().size() >= 3) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static List<Integer> goodDaysToRobBank(int[] security, int time) {
+		ArrayList<Integer> res = new ArrayList<>();
+		if (time == 0) {
+			for (int i = 0; i < security.length; i++) {
+				res.add(i);
+			}
+			return res;
+		}
+		con:
+		for (int i = time; i < security.length - time; i++) {
+			for (int j = i - time + 1; j < i + 1; j++) {
+				if (security[j - 1] < security[j]) {
+					continue con;
+				}
+			}
+			for (int j = i + 1; j < i + time + 1; j++) {
+				if (security[j - 1] > security[j]) {
+					continue con;
+				}
+			}
+			res.add(i);
+		}
+		return res;
+	}
+
+	HashMap<Integer, TreeNode> parMap = new HashMap();
+	TreeNode startNode = null;
+	TreeNode desNode = null;
+	int sta = 0;
+	int end = 0;
+
+	public String getDirections(TreeNode root, int startValue, int destValue) {
+		this.sta = startValue;
+		this.end = destValue;
+		dfs(root);
+		LinkedList<TreeNode> list = new LinkedList<>();
+		StringBuffer sb = new StringBuffer();
+		list.add(startNode);
+		while (!list.isEmpty()) {
+			int size = list.size();
+			while (size > 0) {
+				size--;
+				TreeNode poll = list.poll();
+				if (poll.val == destValue) {
+					break;
+				}
+				if (poll.left != null) {
+
+				}
+				if (poll.right != null) {
+
+				}
+				if (parMap.get(poll.val) != null) {
+
+				}
+			}
+		}
+		return sb.toString();
+	}
+
+	public void dfs(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+		if (node.val == sta) {
+			startNode = node;
+		}
+		if (node.val == end) {
+			desNode = node;
+		}
+		dfs(node.left);
+		dfs(node.right);
+		if (node.left != null) {
+			parMap.put(node.left.val, node);
+		}
+		if (node.right != null) {
+			parMap.put(node.right.val, node);
+		}
+	}
+
+	public void back(int[] nums, LinkedList<Integer> path, int index, LinkedList<List<Integer>> res, boolean[] used) {
+		if (path.size() == 3) {
+			res.add(new ArrayList<>(path));
+			return;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (used[i]) {
+				continue;
+			}
+			used[i] = true;
+			path.add(nums[i]);
+			back(nums, path, index + 1, res, used);
+			path.removeLast();
+			used[i] = false;
+		}
+	}
 
 	public static int countVowelSubstrings(String word) {
 		HashSet<Character> set = new HashSet<>();
